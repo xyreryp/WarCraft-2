dc5222990b0766a6a664cc77971e4c9a1ae71cac
xyreryp <ypxie@ucdavis.edu>
2017-10-04 20:33:38 -0700

    Initial Commit

---
+++ WarCraft2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WarCraft2
+//
+//  Created by Yepu Xie on 10/4/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WarCraft2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WarCraft2
+//
+//  Created by Yepu Xie on 10/4/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
