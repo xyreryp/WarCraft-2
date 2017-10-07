68cadfb6d555c297e8bf62239059270e7cb5f4e4
Sam Shahriary <sshahriary@ucdavis.edu>
2017-10-07 15:52:51 -0700

    Started PlayerAsset.swift, still need to refresh on enum classes

---
+++ WarCraft2/PlayerAsset.swift
@@ -0,0 +1,47 @@
+//
+//  PlayerAsset.swift
+//  Warcraft2
+//
+//  Created by Sam Shahriary on 10/6/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+/*
+**  Player assets file includes Player's assets and data(name, color, gold, capabilities, Upgrades, armor)
+**  C++ to Swift: Internal(default) instead of protected class members,
+**                Initializer instead of class constructor,
+**                removed shared pointer because of swift's memory management
+**                Override instead of virtual functions
+**                Protocols instead of pure virtual functions: throws error if subclass doesn't implement protocol method
+**
+*/
+
+
+import Foundation
+//File inherits from classes Datasource, Position, GameDataTypes
+
+
+protocol CActivatedPlayerCapability {
+    var DActor: CPlayerAsset
+    var DPlayerData: CPlayerData
+    var DTarget: CPlayerAsset
+    
+    public init( actor: CPlayerAsset, playerdata: CPlayerData, target: CPlayerAsset)
+    
+    public override func PercentComplete(max: Int) -> Int
+    public override func IncrementStep() -> bool
+    public override func Cancel()
+    
+}
+
+protocol CPlayerCapability {
+    public:
+        enum ETargetType{
+            case None = 0
+            case Asset
+            case Terrain
+            case TerrainOrAsset
+            case Player
+        }
+}
+
