9ebac5a0a4a2ad9d18178dd05a452a2017e4fa85
Alex Soong <asoong@ucdavis.edu>
2017-10-08 10:19:35 -0700

    first commit for TilePosition.swift

---
+++ WarCraft2/TilePosition.swift
@@ -0,0 +1,13 @@
+//
+//  TilePosition.swift
+//  WarCraft2
+//
+//  Created by Alexander Soong on 10/8/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+class TilePosition : CPosition {
+    
+}
