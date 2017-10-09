2d4e4e5a36903d4254cfff0596ea2948014eceb7
Yepu Xie <12604828+xyreryp@users.noreply.github.com>
2017-10-08 19:58:54 -0700

    fixed the bug

--- WarCraft2/GraphicSurface.swift
+++ WarCraft2/GraphicSurface.swift
@@ -22,17 +22,12 @@ protocol CGraphicSurface {
     func Height() -> Int
     func Format() -> ESurfaceFormat
     func PixelAt(xpos: Int, ypos: Int) -> Int32
+    /*: Clear(xpos _: Int = 0, ypos _: Int = 0, width _: Int = -1, height _: Int = -1) {} */
     func Clear(xpos: Int, ypos: Int, width: Int, height: Int)
-
     func Duplicate() -> CGraphicSurface
     func CreateResourceContext() -> CGraphicResourceContext
-
     func Draw(srcsurface: CGraphicSurface, dxpos: Int, dypos: Int, width: Int, height: Int, sxpos: Int, sypos: Int)
     func Copy(srcsurface: CGraphicSurface, dxpos: Int, dypos: Int, width: Int, height: Int, sxpos: Int, sypos: Int)
     func CopyMaskSurface(srcsurface: CGraphicSurface, dxpos: Int, dypos: Int, masksurface: CGraphicSurface, sxpos: Int, sypos: Int)
     //    func Transform(srcsurface: CGraphicSurface,dxpos: Int, dypos: Int, width: Int, height: Int,  sxpos: Int, sypos: Int, *calldata: Optional, callback: TGraphicSurfaceTransformCallback)
-}
-
-extension CGraphicSurface {
-    func Clear(xpos _: Int = 0, ypos _: Int = 0, width _: Int = -1, height _: Int = -1) {}
 }
