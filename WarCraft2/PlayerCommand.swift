d12f0cb71e86f93c60a9a8b2e6c9354bf41bf85a
aidanbean <ajuengli@ucdavis.edu>
2017-10-09 14:44:57 -0700

    Ported PlayerCommand.h -> PlayerCommand.swift

---
+++ WarCraft2/PlayerCommand.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerCommand.swift
+//  WarCraft2
+//
+//  Created by Aidan Bean on 10/9/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+/// PLAYERCOMMANDREQUEST_TAG
+struct PLAYERCOMMANDREQUEST_TAG {
+    var DAction: EAssetCapabilityType
+    // TODO: update this when CPlayerAsset has been written
+    // weak var DActors: [CPlayerAsset]
+    var DTargetColor: EPlayerColor
+    var DTargetType: EAssetType
+    var DTargetLocation: CPixelPosition
+}
+
+typealias SPlayerCommandRequest = PLAYERCOMMANDREQUEST_TAG
