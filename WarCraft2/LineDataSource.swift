61b548f0337d7dca0268057f59a4981a45e66c3a
Alex Soong <asoong@ucdavis.edu>
2017-10-09 18:58:56 -0700

    commit for PLineDataSource

---
+++ WarCraft2/LineDataSource.swift
@@ -0,0 +1,19 @@
+//
+//  LineDataSource.swift
+//  WarCraft2
+//
+//  Created by Alexander Soong on 10/9/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+protocol PLineDataSource {
+
+    // set/get data
+    var DDataSource: CDataSource { get set }
+
+    // initializer
+    init(source: CDataSource)
+    func Read(line: String) -> Bool
+}
