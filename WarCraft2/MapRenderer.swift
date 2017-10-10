fbb24687e604c096eff0e4cd404050c98a9ec5d0
Alex Soong <asoong@ucdavis.edu>
2017-10-10 16:03:43 -0700

    needs too much from CTerrainMap, will come back later

--- WarCraft2/MapRenderer.swift
+++ WarCraft2/MapRenderer.swift
@@ -7,21 +7,22 @@
 //
 
 import Foundation
+// TODO: COME BACK AFTER I DO CTERRAINMAP
 
 // TODO: implement MapRenderer
 protocol PMapRenderer {
 
     // TODO: uncomment after CGraphicTileset is implemented
-    // var DTileSet: CGraphicTileset {get set}
+    //     var DTileSet: CGraphicTileset {get set}
 
     // TODO: uncomment after CTerrainMap is implemented
-    // var DMap: CTerrainMap {get set}
+    //     var DMap: CTerrainMap {get set}
     var DTileIndices: [[[Int]]] { get set }
     var DPixelIndices: [Int] { get set }
 
     // initializer
     // TODO: uncomment after CGraphicTileset, CTerrainMap is implemented
-    // init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap)
+    //     init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap)
 
     // functions to be implemented in CMapRenderer
     func MapWidth() -> Int
@@ -33,3 +34,52 @@ protocol PMapRenderer {
     func DrawMap(surface: CGraphicSurface, typesurface: CGraphicSurface, rect: SRectangle)
     func DrawMiniMap(surface: CGraphicSurface)
 }
+
+//
+// class CMapRenderer : PMapRenderer{
+//
+//    var DTileIndices: [[[Int]]]
+//
+//    var DPixelIndices: [Int]
+//
+//    // huge constructor
+//    init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap) {
+//        var LineSource:CCommentSkipLineDataSource = CCommentSkipLineDataSource(source: config, commentchar: "#")
+//        var TempString: String = String()
+//        var ItemCount: Int = Int()
+//
+//        // TODO: uncomment after DTileset is implemented
+//        // var tileset: DTileset = DTileset()
+//
+//        // TODO: uncomment after DTileset is implemented
+//        // var map: DMap = DMap()
+//
+//        // TODO: COME BACK AFTER I DO CTERRAINMAP
+//    }
+//
+//
+//    func MapWidth() -> Int {
+//        <#code#>
+//    }
+//
+//    func MapHeight() -> Int {
+//        <#code#>
+//    }
+//
+//    func DetailedMapWidth() -> Int {
+//        <#code#>
+//    }
+//
+//    func DetailedMapHeight() -> Int {
+//        <#code#>
+//    }
+//
+//    func DrawMap(surface: CGraphicSurface, typesurface: CGraphicSurface, rect: SRectangle) {
+//        <#code#>
+//    }
+//
+//    func DrawMiniMap(surface: CGraphicSurface) {
+//        <#code#>
+//    }
+//
+// }
