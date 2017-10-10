29788932d6794a8b9f4b80fcc7b85d5e2ebcf605
Sam Shahriary <sshahriary@ucdavis.edu>
2017-10-09 17:40:48 -0700

    Determining protocol oriented programming. also using Dictionaries instead of unordered maps

--- WarCraft2/PlayerAsset.swift
+++ WarCraft2/PlayerAsset.swift
@@ -7,64 +7,60 @@
 //
 
 /*
-**  Player assets file includes Player's assets and data(name, color, gold, capabilities, Upgrades, armor)
-**  C++ to Swift: Internal(default) instead of protected class members,
-**                Initializer instead of class constructor,
-**                removed shared pointer because of swift's memory management
-**                Override instead of virtual functions
-**                Protocols instead of pure virtual functions: throws error if subclass doesn't implement protocol method
-**                Function parmaters pass by constant by default, inout to pass by reference
-**                Constant member functions: do not modify object in which they are called
-**                Protocols don't allow bodies for functions, use { get set } with in data members to dictate whether they are gettable or settable
-*/
-
+ **  Player assets file includes Player's assets and data(name, color, gold, capabilities, Upgrades, armor)
+ **  C++ to Swift: Internal(default) instead of protected class members,
+ **                Initializer instead of class constructor,
+ **                removed shared pointer because of swift's memory management
+ **                Override instead of virtual functions
+ **                Protocols instead of pure virtual functions: throws error if subclass doesn't implement protocol method
+ **                Function parmaters pass by constant by default, inout to pass by reference
+ **                Constant member functions: do not modify object in which they are called
+ **                Protocols don't allow bodies for functions, use { get set } with in data members to dictate whether they are gettable or settable
+ */
 
 import Foundation
-//File inherits from classes Datasource, Position, GameDataTypes
-
+// File inherits from classes Datasource, Position, GameDataTypes
 
 protocol CActivatedPlayerCapability {
     var DActor: CPlayerAsset
     var DPlayerData: CPlayerData
     var DTarget: CPlayerAsset
-    
-    public init( actor: CPlayerAsset, playerdata: CPlayerData, target: CPlayerAsset)
-    
+
+    public init(actor: CPlayerAsset, playerdata: CPlayerData, target: CPlayerAsset)
+
     public override func PercentComplete(max: Int) -> Int
     public override func IncrementStep() -> bool
     public override func Cancel()
-    
-}
-    //COME BACK TO CPLAYERCAPABILITY
-protocol CPlayerCapability {
-    
-    public enum ETargetType{
-            case None = 0
-            case Asset
-            case Terrain
-            case TerrainOrAsset
-            case Player
-        }
-    //how is it protected?
-    var DName: String { get }       //provides get function for variable DName
-    var DAssetCappabilityType: EAssetCapabilityType { get }
-    var DTargetType: ETargetType { get }
-    
-    init(name: String, targettype: ETargetType)
-    
-    var NameRegistry [String : CPlayerCapability]   //STILL NEED TO DO: STATIC unordered maps REFERENCE
-    var TypeRegistry [Int : CPlayerCapability]
-    
-    //static function
-    
-    AssetCapabilityType() -> EAssetCapabilityType {
-    
-    }
-    
 }
 
-class CPlayerUpgrade{
-    
+// COME BACK TO CPLAYERCAPABILITY
+protocol CPlayerCapability {
+
+    public enum ETargetType {
+        case None = 0
+        case Asset
+        case Terrain
+        case TerrainOrAsset
+        case Player
+    }
+    // how is it protected?
+    var DName: String { get } // provides get function for variable DName
+    var DAssetCappabilityType: EAssetCapabilityType { get }
+    var DTargetType: ETargetType { get }
+
+    init(name: String, targettype: ETargetType)
+
+    var NameRegistry[String: CPlayerCapability] // STILL NEED TO DO: STATIC unordered maps REFERENCE
+    var TypeRegistry[Int: CPlayerCapability]
+
+    // static function
+
+    AssetCapabilityType() -> EAssetCapabilityType {
+    }
+}
+
+class CPlayerUpgrade {
+
     var DName: String { get }
     var DArmor: Int { get }
     var DSight: Int { get }
@@ -75,13 +71,6 @@ class CPlayerUpgrade{
     var DGoldCost: Int { get }
     var DLumberCost: Int { get }
     var DResearchTime: Int { get }
-    
-    var DAffectedAssets : [EAssetType]
-    
-    
-    
-    
-    
-    
-}
 
+    var DAffectedAssets: [EAssetType]
+}
