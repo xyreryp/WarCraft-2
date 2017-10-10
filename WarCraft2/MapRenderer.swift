02639286bffbd13bfc551f80a9b593a5a2651068
Alex Soong <asoong@ucdavis.edu>
2017-10-09 18:50:06 -0700

    protocol for MapRenderer done

--- WarCraft2/MapRenderer.swift
+++ WarCraft2/MapRenderer.swift
@@ -9,6 +9,27 @@
 import Foundation
 
 // TODO: implement MapRenderer
-class CMapRenderer {
-    
+protocol PMapRenderer {
+
+    // TODO: uncomment after CGraphicTileset is implemented
+    // var DTileSet: CGraphicTileset {get set}
+
+    // TODO: uncomment after CTerrainMap is implemented
+    // var DMap: CTerrainMap {get set}
+    var DTileIndices: [[[Int]]] { get set }
+    var DPixelIndices: [Int] { get set }
+
+    // initializer
+    // TODO: uncomment after CGraphicTileset, CTerrainMap is implemented
+    // init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap)
+
+    // functions to be implemented in CMapRenderer
+    func MapWidth() -> Int
+    func MapHeight() -> Int
+    func DetailedMapWidth() -> Int
+    func DetailedMapHeight() -> Int
+
+    // functions to be implemented in CMapRenderer
+    func DrawMap(surface: CGraphicSurface, typesurface: CGraphicSurface, rect: SRectangle)
+    func DrawMiniMap(surface: CGraphicSurface)
 }
