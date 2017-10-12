7d10ceb888bd768dc3931138e2c44da96adb6c7f
Keshav Tirumurti <keshav@campus-094-155.ucdavis.edu>
2017-10-12 14:14:08 -0700

    Handling mouse events for Week 2 Hard Goals

---
+++ WarCraft2/HandlingMouseClicks.swift
@@ -0,0 +1,18 @@
+//
+//  HandlingMouseClicks.swift
+//  WarCraft2
+//
+//  Created by Keshav Tirumurti on 10/12/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Cocoa
+
+class HandlingMouseClicks: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
