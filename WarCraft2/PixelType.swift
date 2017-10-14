87a71caf2d1a57189399c4d4373d452c5ea3537a
Alex Soong <asoong@ucdavis.edu>
2017-10-14 13:36:05 -0700

    first commit for PixelType

---
+++ WarCraft2/PixelType.swift
@@ -0,0 +1,40 @@
+//
+//  PixelType.swift
+//  WarCraft2
+//
+//  Created by Alexander Soong on 10/14/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+protocol PPixelType {
+    enum EAssetTerrainType{
+        case None
+        case Grass
+        case Dirt
+        case Rock
+        case Tree
+        case Stump
+        case Water
+        case Wall
+        case WallDamaged
+        case Rubble
+        case Peasant
+        case Footman
+        case Archer
+        case Ranger
+        case GoldMine
+        case TownHall
+        case Keep
+        case Castle
+        case Farm
+        case Barracks
+        case LumberMill
+        case Blacksmith
+        case ScoutTower
+        case GuardTower
+        case CannonTower
+        case Max
+    }
+}
