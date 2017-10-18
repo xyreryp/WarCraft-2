763159e860fdb0a041d767363d0984822ad2a8f6
Alex Soong <asoong@ucdavis.edu>
2017-10-17 22:09:50 -0700

    finishing up some stuff with tokenizer

--- WarCraft2/MapRenderer.swift
+++ WarCraft2/MapRenderer.swift
@@ -13,16 +13,16 @@ import Foundation
 protocol PMapRenderer {
 
     // TODO: uncomment after CGraphicTileset is implemented
-    //         var DTileSet: CGraphicTileset {get set}
+    var DTileSet: CGraphicTileset {get set}
 
     // TODO: uncomment after CTerrainMap is implemented
-    //         var DMap: CTerrainMap {get set}
+    var DMap: CTerrainMap {get set}
     var DTileIndices: [[[Int]]] { get set }
     var DPixelIndices: [Int] { get set }
 
     // initializer
     // TODO: uncomment after CGraphicTileset, CTerrainMap is implemented
-    //     init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap)
+    init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap)
 
     // functions to be implemented in CMapRenderer
     func MapWidth() -> Int
@@ -36,7 +36,8 @@ protocol PMapRenderer {
 }
 
 
- class CMapRenderer : PMapRenderer{
+class CMapRenderer : PMapRenderer{
+    var DTileSet: CGraphicTileset
 
     var DTileIndices: [[[Int]]]
 
@@ -52,94 +53,94 @@ protocol PMapRenderer {
     }
 
 //    // huge constructor
-//    init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap) {
-//        var LineSource:CCommentSkipLineDataSource = CCommentSkipLineDataSource(source: config, commentchar: "#")
-//        var TempString: String = String()
-//        var ItemCount: Int = Int()
-//
-//        // TODO: uncomment after DTileset is implemented
-////         var tileset: DTileset = DTileset()
-//
-//        // TODO: uncomment after DTileset is implemented
-//        // var map: DMap = DMap()
-//
-//        // TODO: COME BACK AFTER I DO CTERRAINMAP
-//        resize(array: &DPixelIndices, size: ETileType.Max, defaultValue: ETileType.None)
-//        if !LineSource.Read(line: &TempString) {
-//            return
-//        }
-//        ItemCount = Int(TempString)!
-//
-//        var Index: Int = 0
-//        repeat {
-//            var Tokens: [String] = [String]()
-//            if !LineSource.Read(line: &TempString) {
-//                return
-//            }
-//
-//            Tokenize(Tokens, TempString)
-//            var ColorValue: uint32 = Int(Tokens.first)!
-//            var PixelIndex: Int = 0
-//
-//
-//            Index += 1
-//        } while Index < ItemCount
-//
-//
-//    }
-//
-//
-//    func MapWidth() -> Int {
-//        return DMap.Width()
-//    }
-//
-//    func MapHeight() -> Int {
-//        return DMap.Height()
-//    }
-//
-//    func DetailedMapWidth() -> Int {
-//        return DMap.Width() * DTileset.TileWidth()
-//    }
-//
-//    func DetailedMapHeight() -> Int {
-//        return DMap.Height() * DTileset.TileHeight()
-//    }
-//
-//    func DrawMap(surface: CGraphicSurface, typesurface: CGraphicSurface, rect: SRectangle) {
-//        var TileWidth: Int = Int()
-//        var TileHeight: Int = Int()
-//        
-//        TileWidth = DTileset.TileWidth()
-//        TileHeight = DTileset.TileHeight()
-//        
-//        typesurface.Clear(xpos: Int(nil), ypos: Int(nil), width: Int(nil), height: Int(nil))
-//        
-//        var YIndex: Int = rect.DYPosition / TileHeight
-//        var YPos: Int = -(rect.DYPosition % TileHeight)
-//        var XIndex: Int = rect.DXPosition / TileWidth
-//        var XPos: Int = -(rect.DXPosition % TileWidth)
-//        repeat {
-//            repeat {
-//                var PixelType: CPixelType = CPixelType(DMap.TileType(XIndex, YIndex))
-//                
-//                
-//                
-//                
-//                
-//                XIndex += 1
-//                YPos += TileWidth
-//            } while XPos < rect.DWidth
-//            
-//            YIndex += 1
-//            YPos += TileHeight
-//        } while YPos < rect.DHeight
-//        
-//
-//    }
-//
-//    func DrawMiniMap(surface: CGraphicSurface) {
-//        <#code#>
-//    }
-//
+    init(config: CDataSource, tileset: CGraphicTileset, map: CTerrainMap) {
+        var LineSource:CCommentSkipLineDataSource = CCommentSkipLineDataSource(source: config, commentchar: "#")
+        var TempString: String = String()
+        var ItemCount: Int = Int()
+
+        // TODO: uncomment after DTileset is implemented
+//         var tileset: DTileset = DTileset()
+
+        // TODO: uncomment after DTileset is implemented
+        // var map: DMap = DMap()
+
+        // TODO: COME BACK AFTER I DO CTERRAINMAP
+        resize(array: &DPixelIndices, size: ETileType.Max, defaultValue: ETileType.None)
+        if !LineSource.Read(line: &TempString) {
+            return
+        }
+        ItemCount = Int(TempString)!
+
+        var Index: Int = 0
+        repeat {
+            var Tokens: [String] = [String]()
+            if !LineSource.Read(line: &TempString) {
+                return
+            }
+
+            Tokenize(Tokens, TempString)
+            var ColorValue: uint32 = Int(Tokens.first!)
+            var PixelIndex: Int = 0
+
+
+            Index += 1
+        } while Index < ItemCount
+
+
+    }
+
+
+    func MapWidth() -> Int {
+        return DMap.Width()
+    }
+
+    func MapHeight() -> Int {
+        return DMap.Height()
+    }
+
+    func DetailedMapWidth() -> Int {
+        return DMap.Width() * DTileset.TileWidth()
+    }
+
+    func DetailedMapHeight() -> Int {
+        return DMap.Height() * DTileset.TileHeight()
+    }
+
+    func DrawMap(surface: CGraphicSurface, typesurface: CGraphicSurface, rect: SRectangle) {
+        var TileWidth: Int = Int()
+        var TileHeight: Int = Int()
+        
+        TileWidth = DTileset.TileWidth()
+        TileHeight = DTileset.TileHeight()
+        
+        typesurface.Clear(xpos: Int(nil), ypos: Int(nil), width: Int(nil), height: Int(nil))
+        
+        var YIndex: Int = rect.DYPosition / TileHeight
+        var YPos: Int = -(rect.DYPosition % TileHeight)
+        var XIndex: Int = rect.DXPosition / TileWidth
+        var XPos: Int = -(rect.DXPosition % TileWidth)
+        repeat {
+            repeat {
+                var PixelType: CPixelType = CPixelType(DMap.TileType(XIndex, YIndex))
+                
+                
+                
+                
+                
+                XIndex += 1
+                YPos += TileWidth
+            } while XPos < rect.DWidth
+            
+            YIndex += 1
+            YPos += TileHeight
+        } while YPos < rect.DHeight
+        
+
+    }
+
+    func DrawMiniMap(surface: CGraphicSurface) {
+        <#code#>
+    }
+
  }
 
