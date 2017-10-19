e9220043739ddcc37494d4d711ab2cd36601956e
dbendre <dishabendre@gmail.com>
2017-10-18 17:28:34 -0700

    ViewPortX(), ViewPortY() complete

--- WarCraft2/ViewPortRenderer.swift
+++ WarCraft2/ViewPortRenderer.swift
@@ -12,16 +12,17 @@ class CViewportRenderer {
     // protected variables
     
     // c++ shared_ptr
-    internal var DMapRenderer: CMapRenderer // NOTE: type undeclared
-    internal var DAssetRenderer: CAssetRenderer // NOTE: type undeclared
-    internal var DFogRenderer: CFogRenderer // NOTE: type undeclared
+    internal var DMapRenderer: CMapRenderer // NOTE: type undeclared, TODO: MapRenderer class
+    internal var DAssetRenderer: CAssetRenderer // NOTE: type undeclared, TODO: AssetRenderer class
+    internal var DFogRenderer: CFogRenderer // NOTE: type undeclared, TODO: FogRenderer class
     
-    var DViewportX: Int
-    var DViewportY: Int
-    var DLastViewportWidth: Int
-    var DLastViewportHeight: Int
-    // function stubs
-    
+    // c++ protected variables
+    internal var DViewportX: Int
+    internal var DViewportY: Int
+    internal var DLastViewportWidth: Int
+    internal var DLastViewportHeight: Int
+  
+    // constructor
     init(maprender: CMapRenderer, assetrender: CAssetRenderer,
          fogrender: CFogRenderer) {
         DMapRenderer = maprender
@@ -34,7 +35,35 @@ class CViewportRenderer {
     }
     
     deinit {
-        <#statements#>
+        // no statements
+    }
+    
+    func InitViewportDimensions(width: Int, height: Int) { // NOTE: there's no native swift api to set
+                                                           // width and height
+        DLastViewportWidth = width
+        DLastViewportHeight = height
+    }
+    
+    func ViewPortX(x: Int) -> Int { // NOTE: unsure if there's a Swift API for this, SKView?
+        DViewportX = x
+        if DViewportX + DLastViewportWidth >= DMapRenderer.DetailedMapWidth() {
+            DViewportX = DMapRenderer.DetailedMapWidth() - DLastViewportWidth
+        }
+        if 0 > DViewportX {
+            DViewportX = 0
+        }
+        return DViewportX
+    }
+    
+    func ViewPortY(y: Int) -> Int {
+        DViewportY = y
+        if DViewporty + DLastViewportHeight >= DMapRenderer.DetailedMapHeight() {
+            DViewporty = DMapRenderer.DetailedMapHeight() - DLastViewportHeight
+        }
+        if (0 > DViewportY) {
+            DViewportY = 0
+        }
+        return DViewportY
     }
     
 }
