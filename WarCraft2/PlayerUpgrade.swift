1aeb99afde4bd652577dab392adeb2e102ece66c
Yepu Xie <12604828+xyreryp@users.noreply.github.com>
2017-10-18 17:26:12 -0700

    Add playerUpgrade

---
+++ WarCraft2/PlayerUpgrade.swift
@@ -0,0 +1,79 @@
+//
+//  PlayerUpgrade.swift
+//  WarCraft2
+//
+//  Created by Yepu Xie on 10/18/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+//class CPlayerUpgrade{
+//    protected:
+//    std::string DName;
+//    int DArmor;
+//    int DSight;
+//    int DSpeed;
+//    int DBasicDamage;
+//    int DPiercingDamage;
+//    int DRange;
+//    int DGoldCost;
+//    int DLumberCost;
+//    int DResearchTime;
+//    std::vector< EAssetType > DAffectedAssets;
+//    static std::unordered_map< std::string, std::shared_ptr< CPlayerUpgrade > > DRegistryByName;
+//    static std::unordered_map< int, std::shared_ptr< CPlayerUpgrade > > DRegistryByType;
+//
+//    public:
+//    CPlayerUpgrade();
+//
+//    std::string Name() const{
+//    return DName;
+//    };
+//
+//    int Armor() const{
+//    return DArmor;
+//    };
+//
+//    int Sight() const{
+//    return DSight;
+//    };
+//
+//    int Speed() const{
+//    return DSpeed;
+//    };
+//
+//    int BasicDamage() const{
+//    return DBasicDamage;
+//    };
+//
+//    int PiercingDamage() const{
+//    return DPiercingDamage;
+//    };
+//
+//    int Range() const{
+//    return DRange;
+//    };
+//
+//    int GoldCost() const{
+//    return DGoldCost;
+//    };
+//
+//    int LumberCost() const{
+//    return DLumberCost;
+//    };
+//
+//    int ResearchTime() const{
+//    return DResearchTime;
+//    };
+//
+//    const std::vector< EAssetType > &AffectedAssets() const{
+//    return DAffectedAssets;
+//    };
+//
+//    static bool LoadUpgrades(std::shared_ptr< CDataContainer > container);
+//    static bool Load(std::shared_ptr< CDataSource > source);
+//    static std::shared_ptr< CPlayerUpgrade > FindUpgradeFromType(EAssetCapabilityType type);
+//    static std::shared_ptr< CPlayerUpgrade > FindUpgradeFromName(const std::string &name);
+//};
+
