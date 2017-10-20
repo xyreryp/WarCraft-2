a9f6cadcc729af59b619505e08de0f781b1dde16
aidanbean <ajuengli@ucdavis.edu>
2017-10-19 19:08:43 -0700

    Finished porting GraphicMulticolorTileset.  Also, changed some `private` data members in CGraphicTileset to `internal` so i could access them directly from the subclass.

--- WarCraft2/GraphicsTileset.swift
+++ WarCraft2/GraphicsTileset.swift
@@ -12,16 +12,16 @@ import Cocoa
 
 class CGraphicTileset {
     // C++ protected functions
-    private var DSurfaceTileset: CGraphicSurface? // shared pointer variable, strong variable
+    internal var DSurfaceTileset: CGraphicSurface? // shared pointer variable, strong variable
     private var DClippingMasks = [CGraphicSurface]()
     // vector of shared pointers in C++
     private var DMapping = [String: Int]()
     private var DTileNames = [String]()
     private var DGroupNames = [String]()
     private var DGroupSteps = [String: Int]()
-    private var DTileCount: Int
-    private var DTileWidth: Int
-    private var DTileHeight: Int
+    internal var DTileCount: Int
+    internal var DTileWidth: Int
+    internal var DTileHeight: Int
     private var DTileHalfWidth: Int
     private var DTileHalfHeight: Int
     private var DTileSet: [SKNode] = []
