bb50ccbb55487095e107e6486c7f00c2f80ea9c9
dbendre <dishabendre@gmail.com>
2017-10-19 18:57:42 -0700

    finished porting ViewportRenderer, waiting on MapRenderer to be ported in order to test file; any errors in file stem from TODOs for other classes

--- WarCraft2/ViewPortRenderer.swift
+++ WarCraft2/ViewPortRenderer.swift
@@ -12,9 +12,9 @@ class CViewportRenderer {
     // protected variables
     
     // c++ shared_ptr
-    internal var DMapRenderer: CMapRenderer // NOTE: type undeclared, TODO: MapRenderer class
-    internal var DAssetRenderer: CAssetRenderer // NOTE: type undeclared, TODO: AssetRenderer class
-    internal var DFogRenderer: CFogRenderer // NOTE: type undeclared, TODO: FogRenderer class
+    internal var DMapRenderer: CMapRenderer // TODO: MapRenderer.swift
+    internal var DAssetRenderer: CAssetRenderer // TODO: AssetRenderer.swift
+    internal var DFogRenderer: CFogRenderer // TODO: FogRenderer.swift
     
     // c++ protected variables
     internal var DViewportX: Int
@@ -83,14 +83,96 @@ class CViewportRenderer {
     }
     
     func CenterViewport( pos: inout CPixelPosition) {
-        ViewPortX(x: pos.X() - DLastViewportWidth/2)
-        ViewPortY(y: pos.Y() - DLastViewportHeight/2)
+        ViewPortX(x: (pos.X() - DLastViewportWidth/2))
+        ViewPortY(y: (pos.Y() - DLastViewportHeight/2))
     }
     
     func DetailedPosition(pos: inout CPixelPosition) -> CPixelPosition {
-        return CPixelPosition(pos.X() + DViewportX, pos.Y() + DViewportY)
+        return CPixelPosition(x: pos.X() + DViewportX, y: pos.Y() + DViewportY)
     }
     
+    func PanNorth(pan: Int) {
+        DViewportY -= pan
+        if 0 > DViewportY {
+            DViewportY = 0
+        }
+    }
     
+    func PanEast(pan: Int) {
+        ViewPortX(x: (DViewportX + pan))
+    }
     
+    func PanSouth(pan: Int) {
+        ViewPortY(y: (DViewportY + pan))
+    }
+    
+    func PanWest(pan: Int) {
+        DViewportX -= pan
+        if 0 > DViewportX {
+            DViewportX = 0
+        }
+    }
+    
+    func DrawViewport(surface: CGraphicSurface, typesurface: CGraphicSurface,
+                      selectionmarkerlist: inout [CPlayerAsset], // TODO: PlayerAsset.swift
+        selectrect: inout SRectangle, curcapability: EAssetCapabilityType) {
+        var TempRectangle: SRectangle
+        var PlaceType: EAssetType = EAssetType.None
+        var Builder: CPlayerAsset // TODO: CPlayerAsset.swift
+        
+        DLastViewportWidth = surface.Width()
+        DLastViewportHeight = surface.Height()
+        
+        if (DViewportX + DLastViewportWidth >= DMapRenderer.DetailedMapWidth()) {
+            DViewportX = DMapRenderer.DetailedMapWidth() - DLastViewportWidth
+        }
+        if (DViewportY + DLastViewportHeight >= DMapRenderer.DetailedMapHeight()) {
+            DViewportY = DMapRenderer.DetailedMapHeigth() - DLastViewportHeight
+        }
+        
+        TempRectangle.DXPosition = DViewportX
+        TempRectangle.DYPosition = DViewportY
+        TempRectangle.DWidth = DLastViewportWidth
+        TempRectangle.DHeight = DLastViewportHeight
+        
+        switch curcapability {
+        case EAssetCapabilityType.BuildFarm:
+            PlaceType = EAssetType.Farm
+        case EAssetCapabilityType.BuildTownHall:
+            PlaceType = EAssetType.TownHall
+        case EAssetCapabilityType.BuildBarracks:
+            PlaceType = EAssetType.Barracks
+        case EAssetCapabilityType.BuildLumberMill:
+            PlaceType = EAssetType.LumberMill
+        case EAssetCapabilityType.BuildBlacksmith:
+            PlaceType = EAssetType.Blacksmith
+        case EAssetCapabilityType.BuildScoutTower:
+            PlaceType = EAssetType.ScoutTower
+        default:
+            break // do nothing
+        }
+        
+        DMapRenderer.DrawMap(surface, typesurface, TempRectangle)
+        DAssetRenderer.DrawSelections(surface, TempRectangle, selectionmarkerlist,
+                                      selectrect, (EAssetType.None != PlaceType))
+        DAssetRenderer.DrawAssets(surface, typesurface, TempRectangle)
+        DAssetRenderer.DrawOverlays(surface, TempRectangle)
+        
+        // NOTE: May require possible fix later 
+        // C++ code: Builder = selectionmarkerlist.front().lock();
+        if (selectionmarkerlist.count != 0) { // if list is not empty
+            
+            if let tempValue = selectionmarkerlist[0] {
+                Builder = tempValue
+            }
+            else {
+                Builder = nil
+            }
+        }
+        
+        DAssetRenderer.DrawPlacement(surface, TempRectangle,
+                                     CPixelPosition(selectrect.DXPosition,
+                                     selectrect.DYPosition), PlaceType, Builder)
+        DFogRenderer.DrawMap(surface, TempRectangle)
+    }
 }
