4fbdd773c60df78dfb22c8fa883ddd94815a5e25
David Montes <dpmontes@ucdavis.edu>
2017-10-22 15:52:20 -0700

    initial

---
+++ BattleMode.swift
@@ -0,0 +1,13 @@
+//
+//  BattleMode.swift
+//  WarCraft2
+//
+//  Created by David Montes on 10/22/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+class CBattleMode {
+    
+}
