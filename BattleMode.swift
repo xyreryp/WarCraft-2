916ad2e36086f561a2b1f61ac85e737f45bbfe90
David Montes <dpmontes@ucdavis.edu>
2017-10-25 16:17:22 -0700

    temp

--- BattleMode.swift
+++ BattleMode.swift
@@ -190,7 +190,229 @@ class CBattleMode : CApplicationMode {
             }
         }
         
+        context.DReleasedKeys.clear()
         
+        context.DMenuButtonState = CButtonRenderer.EButtonState.None
+        CApplicationData.EUIComponentType ComponentType = context.FindUIComponentType(CPixelPosition(CurrentX, CurrentY))
+        if(CApplicationData.uictViewport == ComponentType){
+            var TempPosition(context.ScreenToDetailedMap(CPixelPosition(CurrentX, CurrentY))): CPixelPosition
+            var ViewPortPosition = context.ScreenToViewport(CPixelPosition(CurrentX, CurrentY)): CPixelPosition
+            var PixelType: CPixelType = CPixelType.GetPixelType(context.DViewportTypeSurface, ViewPortPosition)
+            
+            if(context.DRightClick && !context.DRightDown && context.DSelectedPlayerAssets.size()){
+                var CanMove: Bool = true
+                
+                for Asset in context.DSelectedPlayerAssets {
+                    if(LockedAsset = Asset.lock()){
+                        if(0 == LockedAsset.Speed()){
+                            CanMove = false
+                            break
+                        }
+                    }
+                }
+                if(CanMove){
+                    if(EPlayerColor.None != PixelType.Color()){
+                        // Command is either walk/deliver, repair, or attack
+                        
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Move
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetColor = PixelType.Color()
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType = PixelType.AssetType()
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DActors = context.DSelectedPlayerAssets
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation = TempPosition
+                        if(PixelType.Color() == context.DPlayerColor){
+                            bool HaveLumber = false
+                            bool HaveGold = false
+                            
+                            for(auto &Asset : context.DSelectedPlayerAssets){
+                                if(auto LockedAsset = Asset.lock()){
+                                    if(LockedAsset.Lumber()){
+                                        HaveLumber = true
+                                    }
+                                    if(LockedAsset.Gold()){
+                                        HaveGold = true
+                                    }
+                                }
+                            }
+                            if(HaveGold){
+                                if((EAssetType.TownHall == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)||(EAssetType.Keep == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)||(EAssetType.Castle == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)){
+                                    context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Convey
+                                }
+                            }
+                            else if(HaveLumber){
+                                if((EAssetType.TownHall == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)||(EAssetType.Keep == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)||(EAssetType.Castle == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)||(EAssetType.LumberMill == context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType)){
+                                    context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Convey
+                                }
+                            }
+                            else{
+                                auto TargetAsset = context.DGameModel.Player(context.DPlayerColor).SelectAsset(TempPosition, PixelType.AssetType()).lock()
+                                if((0 == TargetAsset.Speed())&&(TargetAsset.MaxHitPoints() > TargetAsset.HitPoints())){
+                                    context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Repair
+                                }
+                            }
+                        }
+                        else{
+                            context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Attack
+                        }
+                        context.DCurrentAssetCapability = EAssetCapabilityType.None
+                    }
+                    else{
+                        // Command is either walk, mine, harvest
+                        CPixelPosition TempPosition(context.ScreenToDetailedMap(CPixelPosition(CurrentX, CurrentY)))
+                        bool CanHarvest = true
+                        
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Move
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetColor = EPlayerColor.None
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType = EAssetType.None
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DActors = context.DSelectedPlayerAssets
+                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation = TempPosition
+                        
+                        for(auto &Asset : context.DSelectedPlayerAssets){
+                            if(auto LockedAsset = Asset.lock()){
+                                if(!LockedAsset.HasCapability(EAssetCapabilityType.Mine)){
+                                    CanHarvest = false
+                                    break
+                                }
+                            }
+                        }
+                        if(CanHarvest){
+                            if(CPixelType.EAssetTerrainType.Tree == PixelType.Type()){
+                                CTilePosition TempTilePosition
+                                
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Mine
+                                TempTilePosition.SetFromPixel(context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation)
+                                if(CTerrainMap.ETileType.Forest != context.DGameModel.Player(context.DPlayerColor).PlayerMap().TileType(TempTilePosition)){
+                                    // Could be tree pixel, but tops of next row
+                                    TempTilePosition.IncrementY(1)
+                                    if(CTerrainMap.ETileType.Forest == context.DGameModel.Player(context.DPlayerColor).PlayerMap().TileType(TempTilePosition)){
+                                        context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation.SetFromTile(TempTilePosition)
+                                    }
+                                }
+                            }
+                            else if(CPixelType.EAssetTerrainType.GoldMine == PixelType.Type()){
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = EAssetCapabilityType.Mine
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType = EAssetType.GoldMine
+                            }
+                        }
+                        context.DCurrentAssetCapability = EAssetCapabilityType.None
+                    }
+                }
+            }
+            else if(context.DLeftClick){
+                if((EAssetCapabilityType.None == context.DCurrentAssetCapability)||(EAssetCapabilityType.BuildSimple == context.DCurrentAssetCapability)){
+                    if(context.DLeftDown){
+                        context.DMouseDown = TempPosition
+                    }
+                    else{
+                        SRectangle TempRectangle
+                        EPlayerColor SearchColor = context.DPlayerColor
+                        std.list< std.shared_ptr< CPlayerAsset > > PreviousSelections
+                        
+                        for(auto WeakAsset : context.DSelectedPlayerAssets){
+                            if(auto LockedAsset = WeakAsset.lock()){
+                                PreviousSelections.push_back(LockedAsset)
+                            }
+                        }
+                        
+                        TempRectangle.DXPosition = std.min(context.DMouseDown.X(), TempPosition.X())
+                        TempRectangle.DYPosition = std.min(context.DMouseDown.Y(), TempPosition.Y())
+                        TempRectangle.DWidth = std.max(context.DMouseDown.X(), TempPosition.X()) - TempRectangle.DXPosition
+                        TempRectangle.DHeight = std.max(context.DMouseDown.Y(), TempPosition.Y()) - TempRectangle.DYPosition
+                        
+                        if((TempRectangle.DWidth < CPosition.TileWidth())||(TempRectangle.DHeight < CPosition.TileHeight())||(2 == context.DLeftClick)){
+                            TempRectangle.DXPosition = TempPosition.X()
+                            TempRectangle.DYPosition = TempPosition.Y()
+                            TempRectangle.DWidth = 0
+                            TempRectangle.DHeight = 0
+                            SearchColor = PixelType.Color()
+                        }
+                        if(SearchColor != context.DPlayerColor){
+                            context.DSelectedPlayerAssets.clear()
+                        }
+                        if(ShiftPressed){
+                            if(!context.DSelectedPlayerAssets.empty()){
+                                if(auto TempAsset = context.DSelectedPlayerAssets.front().lock()){
+                                    if(TempAsset.Color() != context.DPlayerColor){
+                                        context.DSelectedPlayerAssets.clear()
+                                    }
+                                }
+                            }
+                            context.DSelectedPlayerAssets.splice(context.DSelectedPlayerAssets.end(), context.DGameModel.Player(SearchColor).SelectAssets(TempRectangle, PixelType.AssetType(), 2 == context.DLeftClick))
+                            context.DSelectedPlayerAssets.sort(WeakPtrCompare<CPlayerAsset>)
+                            context.DSelectedPlayerAssets.unique(WeakPtrEquals<CPlayerAsset>)
+                        }
+                        else{
+                            PreviousSelections.clear()
+                            context.DSelectedPlayerAssets = context.DGameModel.Player(SearchColor).SelectAssets(TempRectangle, PixelType.AssetType(), 2 == context.DLeftClick)
+                        }
+                        for(auto WeakAsset : context.DSelectedPlayerAssets){
+                            if(auto LockedAsset = WeakAsset.lock()){
+                                bool FoundPrevious = false
+                                for(auto PrevAsset : PreviousSelections){
+                                    if(PrevAsset == LockedAsset){
+                                        FoundPrevious = true
+                                        break
+                                    }
+                                }
+                                if(!FoundPrevious){
+                                    SGameEvent TempEvent
+                                    TempEvent.DType = EEventType.Selection
+                                    TempEvent.DAsset = LockedAsset
+                                    context.DGameModel.Player(context.DPlayerColor).AddGameEvent(TempEvent)
+                                }
+                            }
+                        }
+                        
+                        
+                        context.DMouseDown = CPixelPosition(-1,-1)
+                    }
+                    context.DCurrentAssetCapability = EAssetCapabilityType.None
+                }
+                else{
+                    auto PlayerCapability = CPlayerCapability.FindCapability(context.DCurrentAssetCapability)
+                    
+                    if(PlayerCapability && !context.DLeftDown){
+                        if(((CPlayerCapability.ETargetType.Asset == PlayerCapability.TargetType())||(CPlayerCapability.ETargetType.TerrainOrAsset == PlayerCapability.TargetType()))&&(EAssetType.None != PixelType.AssetType())){
+                            auto NewTarget = context.DGameModel.Player(PixelType.Color()).SelectAsset(TempPosition, PixelType.AssetType()).lock()
+                            
+                            if(PlayerCapability.CanApply(context.DSelectedPlayerAssets.front().lock(), context.DGameModel.Player(context.DPlayerColor), NewTarget)){
+                                SGameEvent TempEvent
+                                TempEvent.DType = EEventType.PlaceAction
+                                TempEvent.DAsset = NewTarget
+                                context.DGameModel.Player(context.DPlayerColor).AddGameEvent(TempEvent)
+                                
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = context.DCurrentAssetCapability
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DActors = context.DSelectedPlayerAssets
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetColor = PixelType.Color()
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType = PixelType.AssetType()
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation = TempPosition
+                                context.DCurrentAssetCapability = EAssetCapabilityType.None
+                            }
+                        }
+                        else if(((CPlayerCapability.ETargetType.Terrain == PlayerCapability.TargetType())||(CPlayerCapability.ETargetType.TerrainOrAsset == PlayerCapability.TargetType()))&&((EAssetType.None == PixelType.AssetType())&&(EPlayerColor.None == PixelType.Color()))){
+                            auto NewTarget = context.DGameModel.Player(context.DPlayerColor).CreateMarker(TempPosition, false)
+                            
+                            if(PlayerCapability.CanApply(context.DSelectedPlayerAssets.front().lock(), context.DGameModel.Player(context.DPlayerColor), NewTarget)){
+                                SGameEvent TempEvent
+                                TempEvent.DType = EEventType.PlaceAction
+                                TempEvent.DAsset = NewTarget
+                                context.DGameModel.Player(context.DPlayerColor).AddGameEvent(TempEvent)
+                                
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DAction = context.DCurrentAssetCapability
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DActors = context.DSelectedPlayerAssets
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetColor = EPlayerColor.None
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetType = EAssetType.None
+                                context.DPlayerCommands[to_underlying(context.DPlayerColor)].DTargetLocation = TempPosition
+                                context.DCurrentAssetCapability = EAssetCapabilityType.None
+                            }
+                        }
+                        else{
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
         
     
     }
