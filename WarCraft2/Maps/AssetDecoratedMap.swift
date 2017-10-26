b40d1482c776e13916a6721d99916b45e4810cc9
KirinSin <aug.plus@gmail.com>
2017-10-25 23:16:00 -0700

    moved AssetDecoratedMap into Map directory

--- WarCraft2/Maps/AssetDecoratedMap.swift
+++ WarCraft2/Maps/AssetDecoratedMap.swift
@@ -138,7 +138,7 @@ class CAssetDecoratedMap: CTerrainMap {
 
         for var YOff: Int in stride(from: 0, to: size, by: 1) {
             for var XOff: Int in stride(from: 0, to: size, by: 1) {
-                var TileTerrainType = TileType(xindex: pos.X() + XOff,yindex: pos.Y() + YOff)
+                var TileTerrainType = TileType(xindex: pos.X() + XOff, yindex: pos.Y() + YOff)
                 if !CanPlaceOn(type: TileTerrainType) {
                     return false
                 }
@@ -178,7 +178,7 @@ class CAssetDecoratedMap: CTerrainMap {
         return true
     }
 
-    func FindAssetPlacement(placeasset: CPlayerAsset, fromasset: CPlayerAsset, nexttiletarget: CTilePosition)->CTilePosition {
+    func FindAssetPlacement(placeasset: CPlayerAsset, fromasset: CPlayerAsset, nexttiletarget: CTilePosition) -> CTilePosition {
         var TopY: Int, BottomY: Int, LeftX: Int, RightX: Int
         var BestDistance: Int = -1
         var CurDistance: Int
@@ -236,7 +236,7 @@ class CAssetDecoratedMap: CTerrainMap {
             }
             if 0 <= LeftX {
                 var ToY: Int = max(TopY, 0)
-                for var CurY: Int in stride(from: min(BottomY, Height() - 1), to: ToY, by: -1){
+                for var CurY: Int in stride(from: min(BottomY, Height() - 1), to: ToY, by: -1) {
                     if CanPlaceAsset(pos: CTilePosition(x: LeftX, y: CurY), size: placeasset.Size(), ignoreasset: placeasset) {
                         var TempPosition = CTilePosition(x: LeftX, y: CurY)
                         CurDistance = TempPosition.DistanceSquared(pos: nexttiletarget)
@@ -357,13 +357,13 @@ class CAssetDecoratedMap: CTerrainMap {
         var Tokens: [String]
         var TempResourceInit: SResourceInitialization
         var TempAssetInit: SAssetInitialization
-        var ResourceCount: Int , AssetCount: Int
+        var ResourceCount: Int, AssetCount: Int
         var InitialLumber: Int = 400
         var ReturnStatus: Bool = false
         if !LoadMap(source: source) {
             return false
         }
-        if !LineSource.Read(line: &TempString) { //supposingly where try starts
+        if !LineSource.Read(line: &TempString) { // supposingly where try starts
             print("Failed To read map resource count.\n")
             return ReturnStatus
         }
@@ -389,10 +389,10 @@ class CAssetDecoratedMap: CTerrainMap {
             if EPlayerColor.None == TempResourceInit.DColor {
                 InitialLumber = TempResourceInit.DLumber
             }
-        
+
             DResourceInitializationList.append(TempResourceInit)
         }
-        
+
         if !LineSource.Read(line: &TempString) {
             print("Failed to read map asset count.\n")
             return ReturnStatus
@@ -413,7 +413,7 @@ class CAssetDecoratedMap: CTerrainMap {
             TempAssetInit.DColor = EPlayerColor(rawValue: Int(Tokens[1])!)!
             TempAssetInit.DTilePosition.X(x: Int(Tokens[2])!)
             TempAssetInit.DTilePosition.Y(y: Int(Tokens[3])!)
-        
+
             if (0 > TempAssetInit.DTilePosition.X()) || (0 > TempAssetInit.DTilePosition.Y()) {
                 print("Invalid resource position %d (%d, %d).\n", Index, TempAssetInit.DTilePosition.X(), TempAssetInit.DTilePosition.Y())
                 return ReturnStatus
@@ -424,136 +424,138 @@ class CAssetDecoratedMap: CTerrainMap {
             }
             DAssetInitializationList.append(TempAssetInit)
         }
-            
+
         resize(array: &DLumberAvailable, size: DTerrainMap.count, defaultValue: [])
-        for var RowIndex: Int in stride(from: 0, to: DLumberAvailable.count,by: 1) {
+        for var RowIndex: Int in stride(from: 0, to: DLumberAvailable.count, by: 1) {
             resize(array: &DLumberAvailable[RowIndex], size: DTerrainMap[RowIndex].count, defaultValue: Int())
             for var ColIndex: Int in stride(from: 0, to: DTerrainMap[RowIndex].count, by: 1) {
                 if ETerrainTileType.Forest == DTerrainMap[RowIndex][ColIndex] {
-                    DLumberAvailable[RowIndex][ColIndex] = DPartials.count <= RowIndex && DPartials[RowIndex].count <= ColIndex ?InitialLumber : 0
+                    DLumberAvailable[RowIndex][ColIndex] = DPartials.count <= RowIndex && DPartials[RowIndex].count <= ColIndex ? InitialLumber : 0
                 } else {
                     DLumberAvailable[RowIndex][ColIndex] = 0
                 }
             }
         }
-        ReturnStatus = true //supposingly where try ends and where catch begins
+        ReturnStatus = true // supposingly where try ends and where catch begins
     }
-    func CreateInitializeMap()->CAssetDecoratedMap{
-        var ReturnMap: CAssetDecoratedMap = CAssetDecoratedMap();
-    
-        if(ReturnMap.DMap.count != DMap.count){
-            ReturnMap.DTerrainMap = DTerrainMap;
-            ReturnMap.DPartials = DPartials;
-    
+
+    func CreateInitializeMap() -> CAssetDecoratedMap {
+        var ReturnMap: CAssetDecoratedMap = CAssetDecoratedMap()
+
+        if ReturnMap.DMap.count != DMap.count {
+            ReturnMap.DTerrainMap = DTerrainMap
+            ReturnMap.DPartials = DPartials
+
             // Initialize to empty grass
             resize(array: &ReturnMap.DMap, size: DMap.count, defaultValue: [])
             for var Row in ReturnMap.DMap {
                 resize(array: &Row, size: DMap[0].count, defaultValue: CTerrainMap.ETileType.None)
-                for var Cell in Row{
-                    Cell = ETileType.None;
+                for var Cell in Row {
+                    Cell = ETileType.None
                 }
             }
             resize(array: &ReturnMap.DMapIndices, size: DMap.count, defaultValue: [])
-            for var Row in ReturnMap.DMapIndices{
+            for var Row in ReturnMap.DMapIndices {
                 resize(array: &Row, size: DMapIndices[0].count, defaultValue: Int())
-                for var Cell in Row{
-                    Cell = 0;
+                for var Cell in Row {
+                    Cell = 0
                 }
             }
         }
-        return ReturnMap;
+        return ReturnMap
     }
-    
-    //    func CreateVisibilityMap() -> PVisibilityMap{ TODO: something wrong with MaxSight.
-//        return PVisibilityMap(width: Width(),height: Height(),maxvisibility: CPlayerAssetType.MaxSight());
-//    }
 
-    func UpdateMap(vismap:CVisibilityMap , resmap:CAssetDecoratedMap){
-        var Iterator = DAssets[0];
-    
-        if(DMap.count != resmap.DMap.count){
-            DTerrainMap = resmap.DTerrainMap;
-            DPartials = resmap.DPartials;
+    //    func CreateVisibilityMap() -> PVisibilityMap{ TODO: something wrong with MaxSight.
+    //        return PVisibilityMap(width: Width(),height: Height(),maxvisibility: CPlayerAssetType.MaxSight());
+    //    }
+
+    func UpdateMap(vismap: PVisibilityMap, resmap: CAssetDecoratedMap) {
+        var Iterator = DAssets[0]
+
+        if DMap.count != resmap.DMap.count {
+            DTerrainMap = resmap.DTerrainMap
+            DPartials = resmap.DPartials
             resize(array: &DMap, size: resmap.DMap.count, defaultValue: [])
-            for var Row in DMap{
+            for var Row in DMap {
                 resize(array: &Row, size: resmap.DMap[0].count, defaultValue: CTerrainMap.ETileType.None)
-                for var Cell in Row{
-                    Cell = ETileType.None;
+                for var Cell in Row {
+                    Cell = ETileType.None
                 }
             }
             resize(array: &DMapIndices, size: resmap.DMapIndices.count, defaultValue: [])
-            for var Row in DMapIndices{
+            for var Row in DMapIndices {
                 resize(array: &Row, size: resmap.DMapIndices[0].count, defaultValue: Int())
-                for var Cell in Row{
-                    Cell = 0;
+                for var Cell in Row {
+                    Cell = 0
                 }
             }
         }
-        while(Iterator != DAssets[DAssets.count]){
-            var CurPosition:CTilePosition  = Iterator.TilePosition();
-            var AssetSize:Int = Iterator.Size();
-            var RemoveAsset:Bool = false;
-            if((Iterator.Speed() != 0 )|| (EAssetAction.Decay == Iterator.Action()) || (EAssetAction.Attack == Iterator.Action())){  //  Remove all movable units
-                //https://stackoverflow.com/questions/24092712/how-to-remove-an-element-of-a-given-value-from-an-array-in-swift
-                if let itemToRemoveIndex = DAssets.index(where: !{$0 != Iterator}) {
-                    DAssets.remove(at: itemToRemoveIndex)
-                }
-                continue;
+        while Iterator != DAssets[DAssets.count] {
+            var CurPosition: CTilePosition = Iterator.TilePosition()
+            var AssetSize: Int = Iterator.Size()
+            var RemoveAsset: Bool = false
+            if (Iterator.Speed() != 0) || (EAssetAction.Decay == Iterator.Action()) || (EAssetAction.Attack == Iterator.Action()) { //  Remove all movable units
+                // https://stackoverflow.com/questions/24092712/how-to-remove-an-element-of-a-given-value-from-an-array-in-swift
+                for var itemToRemoveIndex: Int in stride(from: 0, to: DAssets.count, by: 1) {
+                    if !(DAssets[itemToRemoveIndex] != Iterator) {
+                        DAssets.remove(at: itemToRemoveIndex)
+                    }
+                }//FIXME: this is essentially array.erase(object).
+                continue
             }
-            for var YOff: Int in stride(from: 0, to: AssetSize, by: 1){
-                var YPos: Int = CurPosition.Y() + YOff;
-                for var XOff: Int in stride (from: 0, to: AssetSize, by: 1){
-                    var XPos: Int = CurPosition.X() + XOff;
+            for var YOff: Int in stride(from: 0, to: AssetSize, by: 1) {
+                var YPos: Int = CurPosition.Y() + YOff
+                for var XOff: Int in stride(from: 0, to: AssetSize, by: 1) {
+                    var XPos: Int = CurPosition.X() + XOff
 
-                    var VisType: ETileVisibility = vismap.TileType(XPos, YPos);
-                    if((CVisibilityMap::ETileVisibility::Partial == VisType)||(CVisibilityMap::ETileVisibility::PartialPartial == VisType)||(CVisibilityMap::ETileVisibility::Visible == VisType)){ // Remove visible so they can be updated
-                    RemoveAsset = EAssetType::None != (*Iterator)->Type();
-                    break;
-                }
-            }
-                if(RemoveAsset){
-                    break;
-                }
-            }
-            if(RemoveAsset){
-                Iterator = DAssets.erase(Iterator);
-                continue;
-            }
-            Iterator++;
-        }
-        for(int YPos = 0; YPos < DMap.size(); YPos++){
-            for(int XPos = 0; XPos < DMap[YPos].size(); XPos++){
-                CVisibilityMap::ETileVisibility VisType = vismap.TileType(XPos-1, YPos-1);
-                if((CVisibilityMap::ETileVisibility::Partial == VisType)||(CVisibilityMap::ETileVisibility::PartialPartial == VisType)||(CVisibilityMap::ETileVisibility::Visible == VisType)){
-                    DMap[YPos][XPos] = resmap.DMap[YPos][XPos];
-                    DMapIndices[YPos][XPos] = resmap.DMapIndices[YPos][XPos];
-                }
-            }
-        }
-        for(auto &Asset : resmap.DAssets){
-            CTilePosition CurPosition = Asset->TilePosition();
-            int AssetSize = Asset->Size();
-            bool AddAsset = false;
-        
-            for(int YOff = 0; YOff < AssetSize; YOff++){
-                int YPos = CurPosition.Y() + YOff;
-                for(int XOff = 0; XOff < AssetSize; XOff++){
-                    int XPos = CurPosition.X() + XOff;
-
-                    CVisibilityMap::ETileVisibility VisType = vismap.TileType(XPos, YPos);
-                    if((CVisibilityMap::ETileVisibility::Partial == VisType)||(CVisibilityMap::ETileVisibility::PartialPartial == VisType)||(CVisibilityMap::ETileVisibility::Visible == VisType)){ // Add visible resources
-                        AddAsset = true;
-                        break;
+                    var VisType: ETileVisibility //= vismap.TileType(XPos, YPos) missing func from VisibilityMap
+                    if (CVisibilityMap: :ETileVisibility:: Partial == VisType) || (CVisibilityMap: :ETileVisibility:: PartialPartial == VisType) || (CVisibilityMap: :ETileVisibility:: Visible == VisType) { // Remove visible so they can be updated
+                        RemoveAsset = EAssetType:: None != (*Iterator) -> Type()
+                        break
                     }
                 }
-                if(AddAsset){
-                    DAssets.push_back(Asset);
-                    break;
+                if RemoveAsset {
+                    break
+                }
+            }
+            if RemoveAsset {
+                Iterator = DAssets.erase(Iterator)
+                continue
+            }
+            Iterator++
+        }
+        for int YPos = 0; YPos < DMap.size(); YPos++ {
+            for int XPos = 0; XPos < DMap[YPos].size(); XPos++ {
+                CVisibilityMap:: ETileVisibility VisType = vismap.TileType(XPos - 1, YPos - 1)
+                if (CVisibilityMap: :ETileVisibility:: Partial == VisType) || (CVisibilityMap: :ETileVisibility:: PartialPartial == VisType) || (CVisibilityMap: :ETileVisibility:: Visible == VisType) {
+                    DMap[YPos][XPos] = resmap.DMap[YPos][XPos]
+                    DMapIndices[YPos][XPos] = resmap.DMapIndices[YPos][XPos]
                 }
             }
         }
-    
-        return true;
-    }
+        for auto &Asset: resmap.DAssets {
+            CTilePosition CurPosition = Asset -> TilePosition()
+            int AssetSize = Asset -> Size()
+            bool AddAsset = false
 
+            for int YOff = 0; YOff < AssetSize; YOff++ {
+                int YPos = CurPosition.Y() + YOff
+                for int XOff = 0; XOff < AssetSize; XOff++ {
+                    int XPos = CurPosition.X() + XOff
+
+                    CVisibilityMap:: ETileVisibility VisType = vismap.TileType(XPos, YPos)
+                    if (CVisibilityMap: :ETileVisibility:: Partial == VisType) || (CVisibilityMap: :ETileVisibility:: PartialPartial == VisType) || (CVisibilityMap: :ETileVisibility:: Visible == VisType) { // Add visible resources
+                        AddAsset = true
+                        break
+                    }
+                }
+                if AddAsset {
+                    DAssets.push_back(Asset)
+                    break
+                }
+            }
+        }
+
+        return true
+    }
 }
