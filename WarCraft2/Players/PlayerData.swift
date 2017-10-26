2ec77b6e6f12c83b6b45731695a68b8ba98c8bd6
Alex Soong <asoong@ucdavis.edu>
2017-10-26 00:23:58 -0700

    dealing with compiler err

--- WarCraft2/Players/PlayerData.swift
+++ WarCraft2/Players/PlayerData.swift
@@ -112,7 +112,7 @@ class CPlayerData {
     func FoodConsumption() -> Int {
         var TotalConsumption: Int = 0
         for WeakAsset in DAssets {
-            var AssetConsumption: Int = WeakAsset.FoodConsumption()
+            let AssetConsumption: Int = WeakAsset.FoodConsumption()
             if 0 < AssetConsumption {
                 TotalConsumption += AssetConsumption
             }
@@ -123,7 +123,7 @@ class CPlayerData {
     func FoodProduction() -> Int {
         var TotalProduction: Int = 0
         for WeakAsset in DAssets {
-            var AssetConsumption: Int = WeakAsset.FoodConsumption()
+            let AssetConsumption: Int = WeakAsset.FoodConsumption()
             if 0 < AssetConsumption {
                 TotalProduction += AssetConsumption
             }
@@ -136,7 +136,7 @@ class CPlayerData {
     //    Assets() return DAssets
     //    AssetTypes() return DAssetTypes
 
-    func CreateMarker(pos: CPixelPosition, addtomap _: Bool) -> CPlayerAsset {
+    func CreateMarker(pos: CPixelPosition, addtomap: Bool) -> CPlayerAsset {
         var NewMarker: CPlayerAsset = (DAssetTypes["None"]?.Construct())!
         var TilePosition: CTilePosition = CTilePosition()
         TilePosition.SetFromPixel(pos: pos)
@@ -304,34 +304,33 @@ class CPlayerData {
         return BestAsset
     }
 
-    // TODO: Need AssetDecoratedMap
-    func FindNearestEnemy(pos _: CPixelPosition, range _: Int) -> CPlayerAsset {
+    func FindNearestEnemy(pos: CPixelPosition, range: Int) -> CPlayerAsset {
         var BestAsset: CPlayerAsset
-        //        var BestDistanceSquared = -1
-        //        var r = range
-        //        if 0 < r {
-        //            r = RangeToDistanceSquared(range: r)
-        //        }
-        //        for Asset in DPlayerMap.Assets() {
-        //            if Asset.Color() != DColor && Asset.Color() != EPlayerColor.None && Asset-IsAlive() {
-        //                var Command = Asset->Command()
-        //                if EAssetAction.Capability == Command.DAction {
-        //                    if Command.DAssetTarget && EAssetAction.Construct == Command.DAssetTarget.Action {
-        //                        continue
-        //                    }
-        //                }
-        //                if EAssetAction.ConveyGold != Command.DAction && EAssetAction.ConveyLumber != Command.DAction && EAssetAction.MineGold != Command.DAction {
-        //                    var CurrentDistance = Asset.ClosestPosition(pos).DistanceSquared(pos)
-        //
-        //                    if 0 > r || CurrentDistance <= r {
-        //                        if -1 == BestDistanceSquared || Currentdistance < BestDistanceSquared {
-        //                            BestDistanceSquared = CurrentDistance
-        //                            BestAsset = Asset
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
+        var BestDistanceSquared = -1
+        var r = range
+        if 0 < r {
+            r = RangeToDistanceSquared(range: r)
+        }
+        for Asset in DPlayerMap.Assets() {
+            if Asset.Color() != DColor && Asset.Color() != EPlayerColor.None && Asset - IsAlive() {
+                var Command = Asset.Command()
+                if EAssetAction.Capability == Command.DAction {
+                    if Command.DAssetTarget && EAssetAction.Construct == Command.DAssetTarget.Action {
+                        continue
+                    }
+                }
+                if EAssetAction.ConveyGold != Command.DAction && EAssetAction.ConveyLumber != Command.DAction && EAssetAction.MineGold != Command.DAction {
+                    var CurrentDistance = Asset.ClosestPosition(pos).DistanceSquared(pos)
+
+                    if 0 > r || CurrentDistance <= r {
+                        if -1 == BestDistanceSquared || Currentdistance < BestDistanceSquared {
+                            BestDistanceSquared = CurrentDistance
+                            BestAsset = Asset
+                        }
+                    }
+                }
+            }
+        }
 
         return BestAsset
     }
@@ -475,7 +474,6 @@ class CPlayerData {
         //        }
     }
 
-    // FIXME:
     func HasUpgrade(upgrade: EAssetCapabilityType) -> Bool {
         if 0 > upgrade.rawValue || DUpgrades.count <= upgrade.rawValue {
             return false
