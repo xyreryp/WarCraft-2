3d4263fb01e687b3263aa913de7bbec001fdc70e
Alex Soong <asoong@ucdavis.edu>
2017-10-28 14:00:01 -0700

    set up DAssetTilesets, testing with peasant. need something from DataSource branch

--- WarCraft2/Players/ApplicationData.swift
+++ WarCraft2/Players/ApplicationData.swift
@@ -52,7 +52,37 @@ class CApplicationData {
     var EGameSessionTypeRef: EGameSessionType = EGameSessionType.gstSinglePlayer
     var EPlayerTypeRef: EPlayerType = EPlayerType.ptNone
     
+    // Data Source, used for all reading of files
+    var DataSource: CDataSource = CDataSource()
+    
+    // array of tilesets for all the assset
+    var DAssetTilesets: [CGraphicMulticolorTileset] = [CGraphicMulticolorTileset]()
+    
+    // map for drawing player
+    var DPlayerRecolorMap: CGraphicRecolorMap = CGraphicRecolorMap()
+    
+    
     init() {
+        
+    }
+    static func resize<T>(array: inout [T], size: Int, defaultValue: T) {
+        while array.count < size {
+            array.append(defaultValue)
+        }
+        while array.count > size {
+            array.removeLast()
+        }
+    }
+    
+    func Activate() {
+        // entry point for reading inall the related tilests
+        
+        // resize to the number of EAssetTypes, from GameDataTypes. Should be 16.
+        CApplicationData.resize(array: &DAssetTilesets, size: EAssetType.Max.rawValue, defaultValue: CGraphicMulticolorTileset())
+        
+        // load tileset for peasant
+        DAssetTilesets[EAssetType.Peasant.rawValue] = CGraphicMulticolorTileset()
+        DAssetTilesets[EAssetType.Peasant.rawValue].LoadTileset(colormap: DPlayerRecolorMap, source: DataSource)
         
     }
 }
