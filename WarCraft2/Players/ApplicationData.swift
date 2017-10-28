7ec91a21d6849f71a43d03de8543737430fc4db5
richardgao12345 <richard.gao12345@gmail.com>
2017-10-28 10:56:50 -0700

    Starting basic ApplicationData file

---
+++ WarCraft2/Players/ApplicationData.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationData.swift
+//  WarCraft2
+//
+//  Created by Richard Gao on 10/28/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+class CApplicationData {
+    
+}
