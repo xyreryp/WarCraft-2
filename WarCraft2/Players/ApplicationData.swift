bada829e35341838136a172d947d09f179ad4c3a
richardgao12345 <richard.gao12345@gmail.com>
2017-10-28 11:24:51 -0700

    created enums

--- WarCraft2/Players/ApplicationData.swift
+++ WarCraft2/Players/ApplicationData.swift
@@ -10,4 +10,48 @@ import Foundation
 
 class CApplicationData {
     
+    enum ECursorType: Int{
+        case ctPointer = 0
+        case ctInspect
+        case ctArrowN
+        case ctArrowE
+        case ctArrowS
+        case ctArrowW
+        case ctTargetOff
+        case ctTargetOn
+        case ctMax
+    }
+    enum EUIComponentType: Int{
+        case uictNone = 0
+        case uictViewport
+        case uictViewportBevelN
+        case uictViewportBevelE
+        case uictViewportBevelS
+        case uictViewportBevelW
+        case uictMiniMap
+        case uictUserDescription
+        case uictUserAction
+        case uictMenuButton
+    }
+    
+    enum EGameSessionType {
+        case gstSinglePlayer
+        case gstMultiPlayerHost
+        case gstMultiPlayerClient
+    }
+    
+    enum EPlayerType: Int{
+        case ptNone = 0
+        case ptHuman
+        case ptAIEasy
+        case ptAIMedium
+        case ptAIHard
+    }
+    var ECursorTypeRef: ECursorType
+    var EUIComponentTypeRef: EUIComponentType
+    var EGameSessionTypeRef: EGameSessionType
+    var EPlayerTypeRef: EPlayerType
+    init() {
+        
+    }
 }
