635e7f0bb8d16e58d765e6df4ed0887507436173
Alex Soong <asoong@ucdavis.edu>
2017-10-28 14:10:17 -0700

    added som paring stuff, and Read func for DataSource

--- WarCraft2/Players/ApplicationData.swift
+++ WarCraft2/Players/ApplicationData.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 class CApplicationData {
-    
-    enum ECursorType: Int{
+
+    enum ECursorType: Int {
         case ctPointer = 0
         case ctInspect
         case ctArrowN
@@ -21,7 +21,8 @@ class CApplicationData {
         case ctTargetOn
         case ctMax
     }
-    enum EUIComponentType: Int{
+
+    enum EUIComponentType: Int {
         case uictNone = 0
         case uictViewport
         case uictViewportBevelN
@@ -33,38 +34,38 @@ class CApplicationData {
         case uictUserAction
         case uictMenuButton
     }
-    
+
     enum EGameSessionType {
         case gstSinglePlayer
         case gstMultiPlayerHost
         case gstMultiPlayerClient
     }
-    
-    enum EPlayerType: Int{
+
+    enum EPlayerType: Int {
         case ptNone = 0
         case ptHuman
         case ptAIEasy
         case ptAIMedium
         case ptAIHard
     }
+
     var ECursorTypeRef: ECursorType = ECursorType.ctPointer
     var EUIComponentTypeRef: EUIComponentType = EUIComponentType.uictNone
     var EGameSessionTypeRef: EGameSessionType = EGameSessionType.gstSinglePlayer
     var EPlayerTypeRef: EPlayerType = EPlayerType.ptNone
-    
+
     // Data Source, used for all reading of files
     var DataSource: CDataSource = CDataSource()
-    
+
     // array of tilesets for all the assset
     var DAssetTilesets: [CGraphicMulticolorTileset] = [CGraphicMulticolorTileset]()
-    
+
     // map for drawing player
     var DPlayerRecolorMap: CGraphicRecolorMap = CGraphicRecolorMap()
-    
-    
+
     init() {
-        
     }
+
     static func resize<T>(array: inout [T], size: Int, defaultValue: T) {
         while array.count < size {
             array.append(defaultValue)
@@ -73,16 +74,15 @@ class CApplicationData {
             array.removeLast()
         }
     }
-    
+
     func Activate() {
         // entry point for reading inall the related tilests
-        
+
         // resize to the number of EAssetTypes, from GameDataTypes. Should be 16.
         CApplicationData.resize(array: &DAssetTilesets, size: EAssetType.Max.rawValue, defaultValue: CGraphicMulticolorTileset())
-        
+
         // load tileset for peasant
         DAssetTilesets[EAssetType.Peasant.rawValue] = CGraphicMulticolorTileset()
         DAssetTilesets[EAssetType.Peasant.rawValue].LoadTileset(colormap: DPlayerRecolorMap, source: DataSource)
-        
     }
 }
