558bcaa1f80662a18dcda3fd38d42b710fc5ae8d
dbendre <dishabendre@gmail.com>
2017-11-07 18:27:46 -0800

    re-made branch UnitActionRenderer

---
+++ WarCraft2/Renderers/UnitActionRenderer.swift
@@ -0,0 +1,16 @@
+//
+//  UnitActionRenderer.swift
+//  WarCraft2
+//
+//  Created by Disha Bendre on 11/7/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Foundation
+
+class CUnitActionRenderer {
+    var DIconTileset: CGraphicTileset
+    
+    init() {}
+    //var DBevel: CBevel
+}
